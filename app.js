dea78c345dbb1f0f6e212c2c9a2876366e2482ed
Deepak Kumar <deepakbedwalb5@gmail.com>
2020-07-02 12:28:06 +0530

    Express generator REST API

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var promotionsRouter = require('./routes/promotionsRouter');
+var leadersRouter = require('./routes/leadersRouter');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter); //mounting router i.e.any request coming to dishes handled by dishRouter
+app.use('/promotions', promotionsRouter);
+app.use('/leaders', leadersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
